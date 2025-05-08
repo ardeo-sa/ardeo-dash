0f15a3d0164b9ef059b94a9c1f56fb1c2297df39
wiktorjurkowski <w.jurkowski@ardeo.com>
2025-05-08 16:54:29 +0100

    fixed incorrect db call

--- app/database/metrics.py
+++ app/database/metrics.py
@@ -3,9 +3,14 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 from app.config import METRICS_DB_URI
 
-metrics_engine = create_engine(METRICS_DB_URI, echo=True)
+
+def get_metrics_engine():
+    """
+    use the above when the database is setup
+
+    """
+    return create_engine(METRICS_DB_URI, echo=True)
+
 MetricsSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=metrics_engine)
-
 Base = declarative_base()
-
 Base.metadata.create_all(bind=metrics_engine)
